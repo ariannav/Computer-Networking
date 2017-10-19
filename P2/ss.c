780a3acc0d188f59e256d5e335d9829e8229fd45
arivacca <ariannav14@gmail.com>
2017-10-19 00:21:56 -0600

    added select to have functionality for last stepping stone, now need to add it for the next ss. Also need to figure out how to listen again when done. Anything else needed?

--- P2/ss.c
+++ P2/ss.c
@@ -7,8 +7,12 @@ ss [-p port]
 
 void ss_begin(int port);
 void get_ip(char* ip);
+void sighandler(int sig);
+
+int sockit;
 
 int main(int argc, char *argv[]){
+
   int port;
   if(argc == 1){
     port = 7014;
@@ -29,11 +33,14 @@ void ss_begin(int port){
   struct sockaddr_in server;
 
   //Creating socket
-  int sockit = socket(AF_INET, SOCK_STREAM, 0);
+  sockit = socket(AF_INET, SOCK_STREAM, 0);
   if(sockit == -1){
     printf("Error creating socket. Exiting ss program.\n");
     exit(1);
   }
+
+  //Signal handler
+  signal(SIGINT, sighandler);
 
   //Making socket reusable.
   setsockopt(sockit, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
@@ -51,88 +58,153 @@ void ss_begin(int port){
       exit(1);
   }
 
-  printf("ss <%s, %s>\n", ip, port);
+  printf("ss <%s, %s>:\n", ip, port);
 
-  //Listening for connection from last SS.
-  if(listen(sockit, 1) < 0){
-    printf("Error listening for incoming connection. Exiting program.\n");
-    close(sockit);
-    exit(1);
+  while(1){
+    //Listening for connection from last SS.
+    if(listen(sockit, 1) < 0){
+      printf("Error listening for incoming connection. Exiting program.\n");
+      close(sockit);
+      exit(1);
+    }
+
+    //Set up & using select.
+    fd_set readfds;
+    int max_sd, activity, i;
+
+    FD_ZERO(&readfds);
+    FD_SET(sockit, &readfds);
+    max_sd = sockit;
+
+    if((activity = select(max_sd+1; &readfds, NULL, NULL< NULL))<0){
+      printf("Select failed. Exiting program.");
+      close(sockit);
+      exit(1);
+    }
+
+    //Create client, and accept connection.
+    struct sockaddr_in client;
+    socklen_t size = sizeof(client);
+    int last_ss_fd;
+
+    //If the sockit is set, then we know that we should accept a connection on there.
+    //At this point, only the previous ss will be connecting to this socket.
+    if(FD_ISSET(sockit, &readfds)){
+      if((last_ss_fd = accept(sockit, (struct sockaddr*) &client, &size)) < 0){
+        printf("Last stepping stone not accepted. Exiting program.\n");
+        close(sockit);
+        exit(1);
+      }
+    }
+
+    //Receive list of stepping stones
+    struct ss_packet packet;
+    if(recv(last_ss_fd, packet, sizeof(packet), 0) < 0){
+      printf("Could not receive packet from last ss. Exiting program.\n");
+      close(sockit);
+      exit(1);
+    }
+
+    //Process packet, and share URL/Future SS list, if num_steps = 0, fetch the file.
+    printf("Request: %s\n", packet.url);
+
+    int cl_empty = 0;
+    if(packet.num_steps == 0){
+      printf("Chainlist is empty.\n");
+      cl_empty = 1;
+    }
+    else{
+      printf("Chainlist is\n");
+    }
+
+    //Printing chainlist.
+    for(int i = 0; i< packet.num_steps; i++){
+      printf("<%s, %s>\n", packet.steps[i].ip, packet.steps[i].port);
+    }
+
+    //Selecting next ss if there are more.
+    if(!cl_empty){
+      //Keep passing on.
+      srand(time(NULL));
+      int r = rand() % num_ss;
+      char ip[50]; strcpy(ip, packet.steps[i].ip);
+      char port[6]; strcpy(port, packet.steps[i].port);
+      printf("Next SS is <%s, %s>\n", ip, port);
+
+      //Remove ss from packet.
+      num_ss--;
+      for(int j = r; j<num_ss; j++){
+          packet.steps[j] = packet.steps[j+1];
+      }
+      packet.num_steps--;
+      //TODO: Go to the next step.
+    }
+    else{
+      //Client list is empty, Process file name
+      char* file_name = strrchr(packet.url, '/');
+      if(file_name == NULL){
+        printf("No file name given, defaulting to index.html");
+        file_name = "index.html";
+      }
+      else{
+        file_name++;
+      }
+
+      //wget(url)
+      char command[550];
+      long int f_size = 0;
+      struct stats file_stats;
+      printf("Issuing wget for file %s\n", file_name);
+      sprintf(command, "wget -q %s", packet.url);
+      system(command);
+
+      //Getting file size.
+      if(stat(file_name, &file_stats) == 0){
+        f_size = file_stats.st_size;
+      }
+      else{
+        printf("No file retrieved. Sending back empty buffer.\n");
+      }
+
+      //Getting file contents.
+      char *file_contents = (char *) malloc(f_size);
+      int open_file = open(file_name, O_RDONLY, (S_IRGRP | S_IWGRP | S_IXGRP));
+      if(read(open_file, file_contents, file_size) < 0){
+        printf("Error reading file read from wget. Exiting program.\n");
+        close(sockit);
+        exit(1);
+      }
+      close(open_file);
+      
+      //Removing file:
+      command = "";
+      sprintf(command, "rm %s", file_name);
+      system(command);
+
+      //Sending size
+      char size[10];
+      f_size = htonl(f_size);\
+      memcpy(size, f_size, sizeof(f_size));
+      if(send(last_ss_fd, size, 10, 0) < 0){
+        printf("Could not send file size. Exiting program.\n");
+        close(sockit);
+        exit(1);
+      }
+
+      //Sending file data.
+      if(send(last_ss_fd, file_contents, f_size, 0) < 0){
+        printf("Could not send file. Exiting program.\n");
+        close(sockit);
+        exit(1);
+      }
+      //Done here!
+      //TODO: Do I need to do anything to listen again?
+    }
   }
-
-  struct sockaddr_in client;
-  socklen_t size = sizeof(client);
-  int last_ss_fd;
-  if((last_ss_fd = accept(sockit, (struct sockaddr*) &client, &size)) < 0){
-    printf("Last stepping stone not accepted. Exiting program.\n");
-    close(sockit);
-    exit(1);
-  }
-
-  //Receive list of stepping stones
-  struct ss_packet packet;
-  if(recv(last_ss_fd, packet, sizeof(packet), 0) < 0){
-    printf("Could not receive packet from last ss. Exiting program.\n");
-    close(sockit);
-    exit(1);
-  }
-
-  //Process packet, and share URL/Future SS list, if num_steps = 0, fetch the file.
-  printf("Request: %s\n", packet.url);
-
-  int cl_empty = 0;
-  if(packet.num_steps == 0){
-    printf("Chainlist is empty.\n");
-    cl_empty = 1;
-  }
-  else{
-    printf("Chainlist is\n");
-  }
-
-  //Printing chainlist.
-  for(int i = 0; i< packet.num_steps; i++){
-    printf("<%s, %s>\n", packet.steps[i].ip, packet.steps[i].port);
-  }
-
-  //Selecting next ss if there are more. 
-
-
-  // fd_set active_fd, read_fd;
-  // FD_ZERO (&active_fd);
-  // FD_SET (sockit, &active_fd);
-  //
-  // while(1){
-  //   read_fd = active_fd;
-  //   if((select(FD_SETSIZE, &read_fd, )))
-  // }
-
-  /*
-  General set-up with a socket. Needs to be a client and a server. One on each machine.
-  Use SELECT() here.
-
-  Print hostname and port it is running on.
-  Listen for connections.
-
-  Connection arrives, reads URL and chain info.
-              If chain info is empty, ss uses system() to issue wget.
-                                      reads file & transmits it back to previous ss
-                                      ss tears down connection, erases local copy
-                                      listens for more requests
-              If chain info not empty, use rand() like in awget.c to select next ss.
-                                      connects to next ss
-                                      remove next ss from chainlist
-                                      sends URL and chain list, like in awget.c
-                                      waits for file
-                                      relays file to previous ss
-                                      tears down connection and goes back to listening.
-
-  Clean up and close connections.
-  */
-
-
-
 }
 
+
+//Get this computer's IP to broadcast it.
 void get_ip(char* ip){
     char hostname[255];
     gethostname(hostname, 255);
@@ -158,5 +230,12 @@ void get_ip(char* ip){
     }
 
     freeaddrinfo(info);
+}
+
+//Handling a signal after ctrl+c
+void sighandler(int sig){
+    printf(" Exiting program, closing socket.\n");
+    close(sockit);
+    exit(1);
 }
 
